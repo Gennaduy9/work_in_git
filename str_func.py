8312864d0a707750b3725bfbb7e822160d1fcc8d
Gennadiy <gubingm@list.ru>
2023-04-21 22:55:26 +0300

    Add a docstring to the function wrote a second unction which is capitalizes the first letters of each word in a string

--- str_func.py
+++ str_func.py
@@ -8,4 +8,20 @@ def uppercase_string(input_string):
 
 string = "Hello, World!"
 upper_string = uppercase_string(string)
-print(upper_string) # выводит "HELLO, WORLD!"
+print(f"{upper_string}\n") # выводит "HELLO, WORLD!"
+
+def capitalize_words(string):
+    """
+    Функция принимает строку и делает заглавными первые буквы каждого слова в ней.
+    :param string: строка
+    :return: строка с заглавными первыми буквами каждого слова
+    """
+    return ' '.join(word.capitalize() for word in string.split())
+
+# Пример использования
+
+print(capitalize_words("hello world")) # Hello World
+print(capitalize_words("the quick brown fox")) # The Quick Brown Fox
+print(capitalize_words("python is awesome")) # Python Is Awesome
+
+help(capitalize_words)
